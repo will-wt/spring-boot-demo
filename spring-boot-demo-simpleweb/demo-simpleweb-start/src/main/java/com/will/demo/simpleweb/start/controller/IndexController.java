336af77b289e970fd74c9bdc02ad015690771e71
Will.WT <willwt@qq.com>
2022-10-20 14:03:27 +0800

    add auto-config

--- spring-boot-demo-simpleweb/demo-simpleweb-start/src/main/java/com/will/demo/simpleweb/start/controller/IndexController.java
+++ spring-boot-demo-simpleweb/demo-simpleweb-start/src/main/java/com/will/demo/simpleweb/start/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.will.demo.simpleweb.start.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Value("${log.basedir}")
+    private String logBasedir;
+
     @GetMapping("/")
     public String index(){
         return "This is a simple web demo with SpringBoot.";
@@ -17,6 +21,11 @@ public class IndexController {
     @GetMapping("/check_health")
     public String checkHealth(){
         return "success";
+    }
+
+    @GetMapping("/log_basedir")
+    public String getLogBasedir(){
+        return logBasedir;
     }
 
 }
